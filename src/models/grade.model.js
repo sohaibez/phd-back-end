ae2665ff631cd0ab7c4aae4e9bbd2fcf7ce4306f
sohaibe zerari <bosssohaibe@gmail.com>
2023-06-10 21:26:37 +0100

    add addGrades functionality

--- src/models/grade.model.js
+++ src/models/grade.model.js
@@ -1,4 +1,6 @@
 import gradeMongo from "./grade.mongo.js";
+import noteMongo from "./note.mongo.js";
+import participantMongo from "./participant.mongo.js";
 
 const getAllGrades = async () => {
     try {
@@ -10,9 +12,30 @@ const getAllGrades = async () => {
     }
 }
 
+const calculateGrade = async (participantCode) => {
+    try {
+        const notes = await noteMongo.find({ participantCode });
+        let grade = 0;
+        for (let note of notes) {
+            grade += note.finaleNote;
+        }
+        grade /= notes.length;
+        return grade;
+    } catch (err) {
+        console.log(err);
+        return null;
+    }
+}
+
 const addGrades = async () => {
     try {
-        
+        const participants = await participantMongo.find();
+        for (let participant of participants) {
+            let participantCode = participant.code;
+            await calculateGrade(participantCode);
+        }
+
+        return getAllGrades();
     } catch (err) {
         console.log(err);
         return null;
@@ -20,5 +43,6 @@ const addGrades = async () => {
 }
 
 export {
-    getAllGrades
+    getAllGrades,
+    addGrades
 }
--- src/routes/grade/grade.controller.js
+++ src/routes/grade/grade.controller.js
@@ -1,5 +1,7 @@
 import {
     getAllGrades,
+    addGrades
+
 } from "../../models/grade.model.js";
 
 const httpsGetAllGrades = async (req, res) => {
@@ -10,7 +12,10 @@ const httpsGetAllGrades = async (req, res) => {
 }
 
 const httpsAddGrades = async (req, res) => {
-    
+    const grades = await addGrades();
+
+    if (!grades) return res.status(400).json({ error: "something went wrong" });
+    return res.status(200).json(grades);
 }
 
 export {
