7dde6f33aeaa9ee31fd6bde6a568352e9080aef6
sohaibe zerari <bosssohaibe@gmail.com>
2023-06-11 02:04:54 +0100

    update auth functionality

--- src/routes/auth/auth.controller.js
+++ src/routes/auth/auth.controller.js
@@ -12,10 +12,12 @@ const httpsLoginHandler = async (req, res) => {
 
     if (!userOrParticipant) return res.status(404).json({ error: "user not found" });
 
-    const isPasswordValid = await bcrypt.compare(password, userOrParticipant.password);
+    const isPasswordValid = bcrypt.compare(password, userOrParticipant.password, (error, isMatch) => {
+        if (error) return res.status(400).json({ error: "something went wrong" });
 
-    if (!isPasswordValid) return null;
-    return res.status(200).json(userOrParticipant);
+        if (!isMatch) return res.status(400).json({ error: "invalid data" });
+        return res.status(200).json(userOrParticipant);
+    });
 }
 
 export {
